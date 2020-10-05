06117aa492d839052af9daa8aad5db5ede865daf
= <brennocesar@live.com>
2020-10-05 15:03:34 -0400

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect }from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Alert from './components/layout/Alert'
+import { Provider } from 'react-redux'
+import store from './store'
+import setAuthToken from './utils/setAuthToken'
+import {loadUser}  from './actions/auth'
 
-function App() {
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Navbar/>
+        <Route exact path='/' component={Landing}/>
+        <section className="container">
+          <Alert/>
+          <Switch>
+            <Route exact path='/register' component={Register}/>
+            <Route exact path='/login' component={Login}/>
+          </Switch>
+        </section>
+      </Router>
+    </Provider>
   );
 }
 
